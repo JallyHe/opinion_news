6800c955ec2e1829e03a0d9d41089d191fa2b845
taozhiiq <760782665@qq.com>
2014-12-22 19:40:32 +0800

    linhao little fix

--- opinion_cal/feature_cal.py
+++ opinion_cal/feature_cal.py
@@ -20,12 +20,12 @@ def cut_words(text):
        output:
            terms: 关键词list
     '''
-    if isinstance(text, str):
+    if not isinstance(text, str):
         raise ValueError("cut words input text must be string")
 
     cx_terms = cut(s, text, cx=True)
 
-    return [term for cx, term in cx_terms if cx in cx_dict and term not in black_words]
+    return [term for term, cx in cx_terms if cx in cx_dict and term not in black_words]
 
 
 def extract_feature(items, title_term_weight=5, content_term_weight=1):
@@ -73,5 +73,7 @@ def extract_feature(items, title_term_weight=5, content_term_weight=1):
 
 if __name__ == '__main__':
     items = [{"feature_title": "TF-IDF的主要思想", "feature_content": "如果某个词或短语在一篇文章中出现的频率TF高，并且在其他文章中很少出现，则认为此词或者短语具有很好的类别区分能力，适合用来分类。TF-IDF实际上是：TF * IDF"}, {"feature_title": "平面设计作品", "feature_content": "网页设计欣赏 三维动画赏析 标志设计欣赏 插画设计作品 建筑设计欣赏 VI设计欣赏 UI设计欣赏 摄影艺术 设计理念"}]
-    extract_feature(items, title_term_weight=5, content_term_weight=1)
+    results = extract_feature(items, title_term_weight=5, content_term_weight=1)
+    for k, v in results.iteritems():
+        print k, v
 
