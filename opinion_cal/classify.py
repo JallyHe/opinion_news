a02e72a3bc54cd62fb77b44f9d0adf570cba0867
taozhiiq <760782665@qq.com>
2014-12-26 23:06:33 +0800

    linhao little fix

--- opinion_cal/classify.py
+++ opinion_cal/classify.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 # User: linhaobuaa
-# Date: 2014-12-21 22:00:00
-# Version: 0.1.0
+# Date: 2014-12-26 22:00:00
+# Version: 0.2.0
 
 
 def subevent_classifier(text, labels, feature_words):
     """input
-           text: 单条文本
+           text: 单条文本, utf-8编码
            labels: 子事件标签，list
-           feature_words: 各子事件的特征词，与上述标签对应，list
+           feature_words: 各子事件的特征词，与上述标签对应，list, [{"word1": count1, "word2": count2}, {"word1": count1, "word2": count2}]
+           其中词以utf-8编码
        output
            单条文本的类别标签
-       example
-           from trace import subevent_classifier
-
     """
     if len(labels) != len(feature_words):
         raise ValueError("labels and feature words list must have same length")
@@ -22,10 +20,10 @@ def subevent_classifier(text, labels, feature_words):
         raise ValueError("text must be encode utf-8")
 
     label_score = dict()
-    for idx, count_words_list in enumerate(feature_words):
-        sum_count = reduce(lambda x, y: x[0] + y[0], count_words_list)
+    for idx, count_words in enumerate(feature_words):
+        sum_count =  sum([count for word, count in count_words.iteritems()])
         weight_count = float(sum_count) * 0.05
-        hit_count = sum([count for count, word in count_words if word in text])
+        hit_count = sum([count for word, count in count_words.iteritems() if word in text])
 
         if hit_count > weight_count:
             label = labels[idx]
@@ -46,4 +44,3 @@ if __name__=="__main__":
     event = Event(eventid)
     subevents = event.getSubEvents()
 
-    print subevents
--- opinion_cal/run.py
+++ opinion_cal/run.py
@@ -1,5 +1,6 @@
 #-*-coding=utf-8-*-
 
+
 import time
 from utils import _default_mongo
 from config import MONGO_DB_NAME, EVENTS_NEWS_COLLECTION_PREFIX, \
@@ -12,18 +13,18 @@ from duplicate import max_same_rate, max_same_rate_shingle
 
 
 if __name__ == '__main__':
-    mongo = _default_mongo(usedb=MONGO_DB_NAME)
     topic = "APEC2014"
     topicid = "54916b0d955230e752f2a94e"
+    mongo = _default_mongo(usedb=MONGO_DB_NAME)
 
     event = Event(topicid)
 
     # 获取子事件
     subevents = event.getSubEvents()
-    feature_words_list = []
     labels_list = []
+    feature_words_list = []
     for subevent in subevents:
-        subeventid = subevent["id"]
+        subeventid = subevent["_id"]
         feature = Feature(subeventid)
 
         # 获取每个子事件最新的特征词
@@ -33,35 +34,24 @@ if __name__ == '__main__':
 
     # 获取话题最新的文本
     results = event.getInfos(START_TS, END_TS)
-    count = 0
-    tb = time.time()
-    t0 = tb
     items = []
     for r in results:
-        r['text4duplicate'] = r['title'] + r["content168"]
-
+        text = r['title'].encode('utf-8') + r['content168'].encode('utf-8')
+        """
         idx, rate, reserve = max_same_rate_shingle(items, r)
         if reserve:
             items.append(r)
         else:
             print idx, rate, reserve, items[idx]['title'], r['title'], items[idx]['content168'], r['content168']
-        t1 = time.time()
-        print t1 - tb
-        tb = t1
+        """
 
         news = News(r["_id"], event.id)
         # 单条文本与各子事件的特征词进行匹配，得到每条文本的簇标签
-        label = subevent_classifier(r['title'].encode('utf-8') + r['content168'].encode('utf-8'), labels_list, feature_words_list)
+        label = subevent_classifier(text, labels_list, feature_words_list)
+        print label
 
         if label == "other":
             label = event.getOtherSubEventID()
 
-        news.update_news_subeventid(label)
-
-        count += 1
-        if count == 100:
-            print count, ' need ', t1 - t0, ' seconds'
-            break
-
-    print count, ' need ', t1 - t0, ' seconds'
+        # news.update_news_subeventid(label)
 
